4062d0f14a0a95db5dc9bcd6c457cac6e72fb19f
Felipe Romero <feliperomero3@outlook.com>
2022-07-13 21:36:26 -0500

    Add IPAddressMiddleware using conventions-based approach.

---
+++ src/IPAddressMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace CityBreaks;
+
+// Middleware that follows the conventions-based approach is created as a singleton
+// when the application first starts up, which means that there is only one instance
+// created for the lifetime of the application.
+// This instance is reused for every request that reaches it.
+public class IPAddressMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public IPAddressMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, ILogger<IPAddressMiddleware> logger)
+    {
+        var ipAddress = context.Connection.RemoteIpAddress;
+
+        logger.LogInformation("Visitor is from {IPAddress}", ipAddress);
+
+        await _next(context);
+    }
+}
+
+public static class Extensions
+{
+    // It is recommended that you create your own extension method on IApplicationBuilder
+    // to register your middleware.
+    public static IApplicationBuilder UseIPAddress(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<IPAddressMiddleware>();
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+using CityBreaks;
+
 System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("en-US");
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,11 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+// You will probably want to register this middleware after the static files middleware
+// so that it doesn’t log the IP address for the same visitor for every requested file.
+app.UseIPAddress();
+
 app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
