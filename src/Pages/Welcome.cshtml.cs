efa329df476a2df7eddad951ce56e4c41f6c1a2c
Felipe Romero <feliperomero3@outlook.com>
2022-07-13 20:22:32 -0500

    Adding a new page.

---
+++ src/Pages/Welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages
+{
+    public class WelcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
