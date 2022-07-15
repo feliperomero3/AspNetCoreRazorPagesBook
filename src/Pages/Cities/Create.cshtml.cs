51a9587342f95759698c068252fe88af13c300de
Felipe Romero <feliperomero3@outlook.com>
2022-07-15 16:27:33 -0500

    Accessing values from Request.Form.

---
+++ src/Pages/Cities/Create.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+
+namespace CityBreaks.Pages.Cities;
+
+public class CreateModel : PageModel
+{
+    public string? Message { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+        var cityName = Request.Form["cityName"];
+
+        if (!StringValues.IsNullOrEmpty(cityName))
+        {
+            Message = $"You submitted {cityName}";
+        }
+    }
+}
