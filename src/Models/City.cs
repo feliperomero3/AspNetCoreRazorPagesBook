ac16263e0b830acf8109c401b83e232455652f98
Felipe Romero <feliperomero3@outlook.com>
2022-07-15 18:15:20 -0500

    Binding Simple Collections.

---
+++ src/Models/City.cs
@@ -0,0 +1,7 @@
+﻿namespace CityBreaks.Models;
+
+public class City
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+}
--- src/Pages/City.cshtml.cs
+++ src/Pages/City.cshtml.cs
@@ -1,14 +1,25 @@
+using CityBreaks.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CityBreaks.Pages
 {
     public class CityModel : PageModel
     {
-        public string? CityName { get; set; }
+        [BindProperty]
+        public List<int?> SelectedCities { get; set; } = new();
+
+        public List<City> Cities = new()
+        {
+            new City { Id = 1, Name = "London" },
+            new City { Id = 2, Name = "Paris" },
+            new City { Id = 3, Name = "New York" },
+            new City { Id = 4, Name = "Rome" },
+            new City { Id = 5, Name = "Dublin" }
+        };
 
         public void OnGet(string cityName)
         {
-            CityName = cityName;
         }
     }
 }
