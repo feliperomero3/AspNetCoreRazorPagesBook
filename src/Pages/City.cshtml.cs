24eb9dbcc69a363dca2c26ec430775c7bc6ab377
Felipe Romero <feliperomero3@outlook.com>
2022-07-14 22:30:18 -0500

    Add route parameters.

---
+++ src/Pages/City.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages
+{
+    public class CityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
