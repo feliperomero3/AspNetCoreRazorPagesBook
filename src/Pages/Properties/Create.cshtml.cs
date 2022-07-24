095c7a3914fc6bf15da7fcb673e7b2e6a626ee4e
Felipe Romero <feliperomero3@outlook.com>
2022-07-24 16:13:03 -0500

    Revise PageModel properties for the create property page with data type and display attributes.

--- src/Pages/Properties/Create.cshtml.cs
+++ src/Pages/Properties/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,15 +10,20 @@ public class CreateModel : PageModel
     public string Name { get; set; } = string.Empty;
 
     [BindProperty]
+    [Display(Name = "Maximum number of Guests")]
     public int MaxNumberOfGuests { get; set; }
 
     [BindProperty]
+    [Display(Name = "Day Rate")]
     public decimal DayRate { get; set; }
 
     [BindProperty]
+    [Display(Name = "Is Smoking Permitted?")]
     public bool IsSmokingPermitted { get; set; }
 
     [BindProperty]
+    [DataType(DataType.Date)]
+    [Display(Name = "Available From")]
     public DateTime AvailableFrom { get; set; }
 
     public void OnGet()
