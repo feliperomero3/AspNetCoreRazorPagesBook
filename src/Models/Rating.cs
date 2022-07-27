ac203f59e46cae17c3913b34bf70089ae56e5289
Felipe Romero <feliperomero3@outlook.com>
2022-07-26 21:56:25 -0500

    Use the DisplayAttribute to change the value presented in the select tag helper on create property page.

--- src/Models/Rating.cs
+++ src/Models/Rating.cs
@@ -1,6 +1,19 @@
-﻿namespace CityBreaks.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityBreaks.Models;
 
 public enum Rating
 {
-    Unrated, OneStar, TwoStar, ThreeStar, FourStar, FiveStar
+    [Display(Name = "Unrated")]
+    Unrated,
+    [Display(Name = "1 Star")]
+    OneStar,
+    [Display(Name = "2 Star")]
+    TwoStar,
+    [Display(Name = "3 Star")]
+    ThreeStar,
+    [Display(Name = "4 Star")]
+    FourStar,
+    [Display(Name = "5 Star")]
+    FiveStar
 }
