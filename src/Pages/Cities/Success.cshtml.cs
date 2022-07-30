8d816b9ee2db52faaf4870e97f84d1f76d7a90ce
Felipe Romero <feliperomero3@outlook.com>
2022-07-29 23:33:06 -0500

    Add create city success page.

---
+++ src/Pages/Cities/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages.Cities
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
