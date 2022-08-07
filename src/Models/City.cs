192c9bb5e7998d543c504647f8ebb2212f7638c8
Felipe Romero <feliperomero3@outlook.com>
2022-08-07 11:48:16 -0500

    Update the City class to include a collection of Properties.

--- src/Models/City.cs
+++ src/Models/City.cs
@@ -5,4 +5,5 @@ public class City
     public int Id { get; set; }
     public string? Name { get; set; }
     public Country? Country { get; set; }
+    public ICollection<Property> Properties { get; set; } = new List<Property>();
 }
