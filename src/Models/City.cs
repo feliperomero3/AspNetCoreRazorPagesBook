de5fbb2f3e0012c97d0779e8a2a32ae88b46063a
Felipe Romero <feliperomero3@outlook.com>
2022-08-07 11:36:47 -0500

    Fully defined one to many relationship between Country and City.

--- src/Models/City.cs
+++ src/Models/City.cs
@@ -2,7 +2,7 @@
 
 public class City
 {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string? Name { get; set; }
     public Country? Country { get; set; }
 }
--- src/Models/Country.cs
+++ src/Models/Country.cs
@@ -5,4 +5,5 @@ public class Country
     public int Id { get; set; }
     public string? CountryName { get; set; }
     public string? CountryCode { get; set; }
+    public ICollection<City> Cities { get; set; } = new List<City>();
 }
