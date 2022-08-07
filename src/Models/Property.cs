589b3e9af7427376d5e6ac203ccb27a5919aba2e
Felipe Romero <feliperomero3@outlook.com>
2022-08-07 11:45:30 -0500

    Add the Property class.

---
+++ src/Models/Property.cs
@@ -0,0 +1,13 @@
+﻿namespace CityBreaks.Models;
+
+public class Property
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public int MaxNumberOfGuests { get; set; }
+    public decimal DayRate { get; set; }
+    public bool IsSmokingPermitted { get; set; }
+    public DateTime AvailableFrom { get; set; }
+    public City? City { get; set; }
+}
