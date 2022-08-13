82f9454baee64dbc4aea3b15eb309b2adc446dad
Felipe Romero <feliperomero3@outlook.com>
2022-08-13 18:06:39 -0500

    Adding DbSets.

--- src/Data/CityBreaksContext.cs
+++ src/Data/CityBreaksContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CityBreaks.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityBreaks.Data;
 
@@ -7,4 +8,8 @@ public class CityBreaksContext : DbContext
     public CityBreaksContext(DbContextOptions options) : base(options)
     {
     }
+
+    public DbSet<City>? Cities { get; set; }
+    public DbSet<Country>? Countries { get; set; }
+    public DbSet<Property>? Properties { get; set; }
 }
