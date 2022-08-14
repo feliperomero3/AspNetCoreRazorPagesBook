2824e309bb49239e465b685c03ad06359110be84
Felipe Romero <feliperomero3@outlook.com>
2022-08-13 19:00:19 -0500

    Configuring the model.

---
+++ src/Data/Configurations/CountryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using CityBreaks.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CityBreaks.Data.Configurations;
+
+public class CountryConfiguration : IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.Property(p => p.CountryName).HasMaxLength(64);
+        builder.Property(p => p.CountryCode).HasMaxLength(2);
+    }
+}
