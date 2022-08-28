627d1e75bb254b04dd3a5d20e1d5b6f54ddcdc28
Felipe Romero <feliperomero3@outlook.com>
2022-08-27 21:04:25 -0500

    Add Image property to the City class.

--- src/Models/City.cs
+++ src/Models/City.cs
@@ -4,6 +4,7 @@ public class City
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    public string? Image { get; set; }
     public Country? Country { get; set; }
     public ICollection<Property> Properties { get; set; } = new List<Property>();
 }
