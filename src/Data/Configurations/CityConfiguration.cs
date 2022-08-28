564a9589bca9b4e7d66ad5c309700a70415ec099
Felipe Romero <feliperomero3@outlook.com>
2022-08-27 21:16:39 -0500

    Add City seed data.

--- src/Data/Configurations/CityConfiguration.cs
+++ src/Data/Configurations/CityConfiguration.cs
@@ -10,5 +10,21 @@ public class CityConfiguration : IEntityTypeConfiguration<City>
     {
         builder.Property(p => p.Name).HasMaxLength(64);
         builder.Property(p => p.Image).HasMaxLength(256);
+
+        builder.HasData(new List<City>
+        {
+            new City { Id = 1, Name = "Amsterdam", CountryId = 5, Image = "amsterdam.jpg" },
+            new City { Id = 2, Name = "Barcelona", CountryId = 7, Image ="barcelona.jpg" },
+            new City { Id = 3, Name = "Berlin", CountryId = 4, Image = "berlin.jpg" },
+            new City { Id = 4, Name = "Copenhagen", CountryId = 2, Image = "copenhagen.jpg" },
+            new City { Id = 5, Name = "Dubrovnik", CountryId = 1, Image = "dubrovnik.jpg" },
+            new City { Id = 6, Name = "Edinburgh", CountryId = 8, Image = "edinburgh.jpg" },
+            new City { Id = 7, Name = "London", CountryId = 8, Image = "london.jpg" },
+            new City { Id = 8, Name = "Madrid", CountryId = 7, Image = "madrid.jpg" },
+            new City { Id = 9, Name = "New York", CountryId = 9, Image = "new-york.jpg" },
+            new City { Id = 10, Name = "Paris", CountryId = 3, Image = "paris.jpg" },
+            new City { Id = 11, Name = "Rome", CountryId = 6, Image = "rome.jpg" },
+            new City { Id = 12, Name = "Venice", CountryId = 6, Image = "venice.jpg" }
+        });
     }
 }
--- src/Models/City.cs
+++ src/Models/City.cs
@@ -5,6 +5,7 @@ public class City
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Image { get; set; }
+    public int CountryId { get; set; }
     public Country? Country { get; set; }
     public ICollection<Property> Properties { get; set; } = new List<Property>();
 }
