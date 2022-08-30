fa02b1e45cf0ea971501cdb1436a80c00385b4df
Felipe Romero <feliperomero3@outlook.com>
2022-08-29 20:58:21 -0500

    Revise IndexModel code for the homepage.

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,53 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using CityBreaks.Models;
+using CityBreaks.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CityBreaks.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly CityService _cityService;
+
+    public IndexModel(CityService cityService)
+    {
+        _cityService = cityService;
+    }
+
     [BindProperty]
     [Display(Name = "Cities")]
     public int[] SelectedCities { get; set; } = new[] { 0 };
 
-    public SelectList? Cities { get; set; }
+    public List<City>? Cities { get; set; }
 
     public string Message { get; set; } = string.Empty;
 
-    public void OnGet()
+    public async void OnGet()
     {
-        Cities = GetCityOptions();
+        Cities = await _cityService.GetAllAsync();
     }
 
     public void OnPost()
     {
-        Cities = GetCityOptions();
-
-        if (ModelState.IsValid)
-        {
-            var cityIds = SelectedCities.Select(x => x.ToString());
-            var cities = GetCityOptions().Where(o => cityIds.Contains(o.Value)).Select(o => o.Text);
-
-            Message = $"You selected {string.Join(", ", cities)}.";
-        }
-    }
-
-    private static SelectList GetCityOptions()
-    {
-        var cities = new City[]
-        {
-            new City { Id = 1, Name = "London" },
-            new City { Id = 2, Name = "New York" },
-            new City { Id = 3, Name = "Paris" },
-            new City { Id = 4, Name = "Berlin" },
-            new City { Id = 5, Name = "Rome" },
-            new City { Id = 6, Name = "Dublin" }
-        };
-
-        var citiesOptions = new SelectList(cities, nameof(City.Id), nameof(City.Name));
-
-        return citiesOptions;
     }
 }
