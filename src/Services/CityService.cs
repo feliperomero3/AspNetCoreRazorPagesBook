5972605a2bda858dde6631fe79d0c88474000e3f
Felipe Romero <feliperomero3@outlook.com>
2022-08-29 20:50:08 -0500

    Add the CityService class.

---
+++ src/Services/CityService.cs
@@ -0,0 +1,22 @@
+﻿using CityBreaks.Data;
+using CityBreaks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Services;
+
+public class CityService
+{
+    private readonly CityBreaksContext _context;
+
+    public CityService(CityBreaksContext context) => _context = context;
+
+    public async Task<List<City>> GetAllAsync()
+    {
+        var cities = await _context.Cities!
+            .Include(c => c.Country)
+            .Include(c => c.Properties.Where(p => p.AvailableFrom < DateTime.Now))
+            .ToListAsync();
+
+        return cities;
+    }
+}
