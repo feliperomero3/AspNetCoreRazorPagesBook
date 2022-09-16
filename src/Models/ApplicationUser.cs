3bed6fbc726bf7ec16ccd38f1ed5f1864efcad3e
Felipe Romero <feliperomero3@outlook.com>
2022-09-16 10:58:05 -0500

    Customizing Identity — Customizing the user.

--- src/Models/ApplicationUser.cs
+++ src/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace CityBreaks.Models;
 
 public class ApplicationUser : IdentityUser
 {
-
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Address { get; set; }
 }
