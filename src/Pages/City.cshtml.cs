3f0f80721573300a4ad9a6209208369fccf9e0e9
Felipe Romero <feliperomero3@outlook.com>
2023-04-21 22:50:31 -0600

    Calling page handlers from JavaScript — Posting to a page handler.

--- src/Pages/City.cshtml.cs
+++ src/Pages/City.cshtml.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
 using CityBreaks.Models;
 using CityBreaks.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +23,20 @@ namespace CityBreaks.Pages
 
         public City? City { get; set; }
 
+        public class BookingInputModel
+        {
+            public Property? Property { get; set; }
+
+            [Display(Name = "Number of guests")]
+            public int NumberOfGuests { get; set; }
+
+            [DataType(DataType.Date), Display(Name = "Arrival")]
+            public DateTime? StartDate { get; set; }
+
+            [DataType(DataType.Date), Display(Name = "Departure")]
+            public DateTime? EndDate { get; set; }
+        }
+
         public async Task<ActionResult> OnGet()
         {
             City = await _cityService.GetByNameAsync(Name);
@@ -43,6 +59,31 @@ namespace CityBreaks.Pages
             }
 
             return Partial("_PropertyDetailsPartial");
+        }
+
+        public async Task<ContentResult> OnPostBooking(BookingInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Content("Invalid property.");
+            }
+
+            if (model.Property is null || model.StartDate is null || model.EndDate is null)
+            {
+                return Content("$0.00", MediaTypeNames.Text.Plain);
+            }
+
+            var property = await _propertyService.GetByIdAsync(model.Property.Id);
+
+            if (property is null)
+            {
+                return Content("$0.00", MediaTypeNames.Text.Plain);
+            }
+
+            var numberOfDays = (int)(model.EndDate - model.StartDate).Value.TotalDays;
+            var totalCost = numberOfDays * property.DayRate * model.NumberOfGuests;
+
+            return Content(totalCost.ToString("C"), MediaTypeNames.Text.Plain);
         }
     }
 }
--- src/wwwroot/js/city.js
+++ src/wwwroot/js/city.js
@@ -5,4 +5,10 @@ $(function () {
     const id = $(this).data('id');
     $('.modal-body').load(`?handler=PropertyDetails&id=${id}`);
   });
+  $('#book').on('click', function () {
+    const data = $('#booking-form').serialize();
+    $.post('?handler=booking', data, function (totalCost) {
+      alert(`Your stay will cost ${totalCost}`);
+    });
+  });
 });
