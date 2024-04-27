806aa5e64dec48b0bca0155f7ca6988ed2d793e7
Felipe Romero <feliperomero3@outlook.com>
2024-04-27 00:46:51 -0600

    Add cities view component.

---
+++ src/Pages/Shared/Components/Cities/CitiesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using CityBreaks.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityBreaks.Pages.Shared.Components.Cities;
+
+public class CitiesViewComponent : ViewComponent
+{
+    private readonly CityService _cityService;
+    private readonly ILogger<CitiesViewComponent> _logger;
+
+    public CitiesViewComponent(CityService cityService, ILogger<CitiesViewComponent> logger)
+    {
+        _cityService = cityService ?? throw new ArgumentNullException(nameof(cityService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var cities = await _cityService.GetAllAsync();
+
+        _logger.LogInformation("Returning {Count} cities.", cities.Count);
+
+        return View(cities);
+    }
+}
