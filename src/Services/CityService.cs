101832155701d50cb1190e095266afb83e618dbe
Felipe Romero <feliperomero3@outlook.com>
2024-05-02 23:38:08 -0600

    Add GetCityNamesAsync method using caching.

--- src/Services/CityService.cs
+++ src/Services/CityService.cs
@@ -1,14 +1,24 @@
 ﻿using CityBreaks.Data;
 using CityBreaks.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CityBreaks.Services;
 
 public class CityService
 {
-    private readonly CityBreaksContext _context;
+    private const string CityNamesCacheKey = nameof(GetCityNamesAsync);
 
-    public CityService(CityBreaksContext context) => _context = context;
+    private readonly IMemoryCache _cache;
+    private readonly CityBreaksContext _context;
+    private readonly ILogger<CityBreaksContext> _logger;
+
+    public CityService(CityBreaksContext context, IMemoryCache cache, ILogger<CityBreaksContext> logger)
+    {
+        _context = context;
+        _cache = cache;
+        _logger = logger;
+    }
 
     public async Task<List<City>> GetAllAsync()
     {
@@ -23,14 +33,31 @@ public class CityService
 
     public async Task<City?> GetByNameAsync(string name)
     {
-        if (name is null)
-        {
-            throw new ArgumentNullException(nameof(name));
-        }
+        if (name is null) throw new ArgumentNullException(nameof(name));
 
         return await _context.Cities!
             .Include(c => c.Country)
             .Include(c => c.Properties.Where(p => p.AvailableFrom < DateTime.Now))
             .SingleOrDefaultAsync(c => c.Name == name);
+    }
+
+    public async Task<List<string>> GetCityNamesAsync()
+    {
+        var isCacheHit = _cache.TryGetValue(CityNamesCacheKey, out List<string> cachedCityNames);
+
+        if (isCacheHit)
+        {
+            _logger.LogInformation("Returning {Count} city names from cache.", cachedCityNames.Count);
+
+            return cachedCityNames;
+        }
+
+        var cityNames = await _context.Cities.Select(c => c.Name!).ToListAsync();
+
+        _cache.Set(CityNamesCacheKey, cityNames);
+
+        _logger.LogInformation("Returning {Count} city names from database.", cityNames.Count);
+
+        return cityNames;
     }
 }
